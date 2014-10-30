5fdad6582a3912479cadbffd31a76f5e802be4a4
Daniel Jost <torpedojost@gmail.com>
2014-10-29 21:30:22 -0400

    Took files from https://github.com/alanstevens/ChocoPackages.

---
+++ NewPackage.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+:: NewPackage.cmd NameOfPackage
+
+setlocal enableextensions
+set OLDDIR=%CD%
+:: Change to the directory containing the script
+cd /d %0\..
+
+md %1
+md %1\tools
+copy nuspec.template.xml %1\%1.nuspec
+copy ChocolateyInstall.template.ps1 %1\tools\ChocolateyInstall.ps1
+
+:: Return to the original directory
+cd /d %OLDDIR%
