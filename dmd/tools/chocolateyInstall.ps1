50831c85aac2adff83f7b2ada03d7646ce5bea50
Daniel Jost <daniel@danieljost.com>
2015-02-12 21:17:22 -0800

    Fixed the dmd download link.

--- dmd/tools/chocolateyInstall.ps1
+++ dmd/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'dmd'
 $version = '2.066.1'
-$url = 'http://downloads.dlang.org/releases/{0}/dmd.{0}.windows.zip' -f $version
+$url = 'http://downloads.dlang.org/releases/2.x/{0}/dmd.{0}.windows.zip' -f $version
 $installDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
