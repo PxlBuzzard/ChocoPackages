4e048c2cb91e03bc9fb3fa161cfdcb371bc4a0f6
Daniel Jost <torpedojost@gmail.com>
2015-03-24 21:34:31 -0700

    Updated dmd to v2.067.0

--- dmd/tools/chocolateyInstall.ps1
+++ dmd/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'dmd'
-$version = '2.066.1'
+$version = '2.067.0'
 $url = 'http://downloads.dlang.org/releases/2.x/{0}/dmd.{0}.windows.zip' -f $version
 $installDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
