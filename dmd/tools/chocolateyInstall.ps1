246232f63e5e1a5ccadc8cf6e43ff91a8279888a
Daniel Jost <torpedojost@gmail.com>
2017-01-31 21:35:12 -0800

    Added checksum to dmd.

--- dmd/tools/chocolateyInstall.ps1
+++ dmd/tools/chocolateyInstall.ps1
@@ -1,6 +1,12 @@
 $packageName = 'dmd'
 $version = '2.073.0'
+$checksum = 'F3B9AD363463775223561B2D9A2EF8D83CC1585020107C7DF8F3D3EB4132BAFB'
 $url = 'http://downloads.dlang.org/releases/2.x/{0}/dmd.{0}.windows.zip' -f $version
 $installDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
+Install-ChocolateyZipPackage 
+-PackageName "$packageName"
+-Url "$url" 
+-UnzipLocation "$installDir"
+-Checksum "$checksum"
+-ChecksumType "sha256"
