e3ef9a4e97a23fbbf5758ed774593e0efab74eef
Daniel Jost <torpedojost@gmail.com>
2017-01-31 22:59:10 -0800

    Fixed spacing in dmd.

--- dmd/tools/chocolateyInstall.ps1
+++ dmd/tools/chocolateyInstall.ps1
@@ -4,9 +4,4 @@ $checksum = 'F3B9AD363463775223561B2D9A2EF8D83CC1585020107C7DF8F3D3EB4132BAFB'
 $url = 'http://downloads.dlang.org/releases/2.x/{0}/dmd.{0}.windows.zip' -f $version
 $installDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
-Install-ChocolateyZipPackage 
--PackageName "$packageName"
--Url "$url" 
--UnzipLocation "$installDir"
--Checksum "$checksum"
--ChecksumType "sha256"
+Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -UnzipLocation "$installDir" -Checksum "$checksum" -ChecksumType "sha256"
