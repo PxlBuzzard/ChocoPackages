38e0ae183295beb10babe8ffcfad3e9a57d962f6
Daniel Jost <torpedojost@gmail.com>
2017-04-05 23:12:35 -0700

    Updated dub to 1.2.1.

--- dub/tools/chocolateyInstall.ps1
+++ dub/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'dub'
-$version = '1.2.0'
-$url = 'http://code.dlang.org/files/dub-{0}-windows-x86.zip' -f $version
+$version = '1.2.1'
+$url = 'https://code.dlang.org/files/dub-{0}-windows-x86.zip' -f $version
 $installDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
