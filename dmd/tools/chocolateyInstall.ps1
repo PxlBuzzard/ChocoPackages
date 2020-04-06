c8f2fa0c719dc70018a30804e25923b2e5c336fa
PxlBuzzard <daniel@danieljost.com>
2020-04-06 14:26:13 -0700

    Updated dmd to 2.091.0

--- dmd/tools/chocolateyInstall.ps1
+++ dmd/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   fileType       = 'exe'
   softwareName   = 'dmd'
 
-  checksum       = 'adbc22e0dd79dce6980e93dfbf78abb7661e40d573b2ca18d9969fa1468a3fd2'
+  checksum       = '60fd18e19062979e987a82b667bb6ac666a3de440eaa1e0253498dd518276245'
   checksumType   = 'sha256'
-  url            = 'http://downloads.dlang.org/releases/2.x/2.090.0/dmd-2.090.0.exe'
+  url            = 'http://downloads.dlang.org/releases/2.x/2.091.0/dmd-2.091.0.exe'
 
   silentArgs     = '/S'
   validExitCodes = @(0)
