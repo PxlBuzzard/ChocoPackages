6836961d8c47f4b89042a4cf4406b07ab070e7a4
PxlBuzzard <daniel@danieljost.com>
2026-08-21 23:25:51 -0700

    Fix nightly Chocolatey publishing

--- dub/push.ps1
+++
@@ -1,8 +0,0 @@
-$packages = Get-ChildItem -Path $PSScriptRoot -Filter 'dub.*.nupkg' -File
-
-foreach ($package in $packages) {
-  choco push $package.FullName --source https://push.chocolatey.org/
-  if ($LASTEXITCODE -ne 0) {
-    throw "Failed to push $($package.Name)."
-  }
-}
